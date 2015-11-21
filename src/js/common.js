ba80ba44d3cb5fe3e6ccb20f66dfc9dc43b0f818
veremey <veremey@i.ua>
2015-11-21 16:38:09 +0200

    listing

--- src/js/common.js
+++ src/js/common.js
@@ -25,40 +25,72 @@ $(document).ready(function() {
 
 	// check select
 
-		function select() {
-			$(".js-select").each(function(){
-				var select_list = $(this).parent().find(".js-select-list");
-				var text = select_list.find("li").first().text();
-				$(this).find(".js-select-text").text(text);
-				$(this).click(function(event){
-					if ($(this).hasClass("is-active")) {
-					    $(this).removeClass("is-active");
-					    select_list.slideUp("fast");
-					}
-					else {
-					    $(".js-select").removeClass("is-active");
-					    $(".js-select-list").hide();
-					    select_list.slideDown("fast");
-					    $(this).addClass("is-active");
-					}
-					event.stopPropagation();
-				});
-				select_list.find("li").click(function(event) {
-					var id = $(this).attr("data-id");
-					var text = $(this).text();
-					$(this).parent().parent().find(".js-select-text").text(text);
-					$(this).parent().parent().find(".js-select-input").val(id);
-					$(this).parent().hide();
-					$(this).parents(".js-select").removeClass("is-active");
-					event.stopPropagation();
-				});
+	function select() {
+		$(".js-select").each(function(){
+			var select_list = $(this).parent().find(".js-select-list");
+			var text = select_list.find("li").first().text();
+			$(this).find(".js-select-text").text(text);
+			$(this).click(function(event){
+				if ($(this).hasClass("is-active")) {
+				    $(this).removeClass("is-active");
+				    select_list.slideUp("fast");
+				}
+				else {
+				    $(".js-select").removeClass("is-active");
+				    $(".js-select-list").hide();
+				    select_list.slideDown("fast");
+				    $(this).addClass("is-active");
+				}
+				event.stopPropagation();
 			});
-		}
-		select();
-
-		$('.js-select').click(function(event){
-		    event.stopPropagation();
+			select_list.find("li").click(function(event) {
+				var id = $(this).attr("data-id");
+				var text = $(this).text();
+				$(this).parent().parent().find(".js-select-text").text(text);
+				$(this).parent().parent().find(".js-select-input").val(id);
+				$(this).parent().hide();
+				$(this).parents(".js-select").removeClass("is-active");
+				event.stopPropagation();
+			});
 		});
+	}
+	select();
+
+	$('.js-select').click(function(event){
+	    event.stopPropagation();
+	});
+
+
+	// slick carousel
+
+	$('.js-carousel').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		fade: true,
+		asNavFor: '.js-carousel-preview',
+	});
+	$('.js-carousel-preview').slick({
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		asNavFor: '.js-carousel',
+		dots: false,
+		arrows: true,
+		infinite: false,
+		centerMode: false,
+		focusOnSelect: true,
+	});
+
+	$(".js-carousel-preview .slick-slide").on("click",function (){
+	  $(this).parent().find(".slick-slide").removeClass("is-active");
+	  $(this).addClass("is-active")
+	  return false;
+	});
+
+
+
+
+
 
 
 
